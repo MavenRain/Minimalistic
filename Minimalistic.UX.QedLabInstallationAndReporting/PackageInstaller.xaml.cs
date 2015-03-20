6e94b186339e6d9de1542024c17a502a9121f00b
U-REDMOND\v-oniobi <v-oniobi@v-oniobi-1217.redmond.corp.microsoft.com>
2015-03-20 15:23:53 -0700

    QED Lab UI prototype began.

---
+++ Minimalistic.UX.QedLabInstallationAndReporting/PackageInstaller.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minimalistic.UX.QedLabInstallationAndReporting
+{
+    /// <summary>
+    /// Interaction logic for PackageInstaller.xaml
+    /// </summary>
+    public partial class PackageInstaller : Window
+    {
+        public PackageInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
